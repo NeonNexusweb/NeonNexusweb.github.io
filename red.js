97c75e5bc2fafcfa44242f56ef1da879df86cf67
Server <giovannipasulo12@gmail.com>
2026-02-08 00:11:36 +0100

    Delete red.js

--- red.js
+++
@@ -1,9 +0,0 @@
-const box = document.querySelector(".leaves");
-
-for (let i = 0; i < 30; i++) {
-  const s = document.createElement("span");
-  s.textContent = "🍁";
-  s.style.left = Math.random() * 100 + "vw";
-  s.style.animationDuration = 6 + Math.random() * 10 + "s";
-  box.appendChild(s);
-}
